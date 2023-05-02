c8888c4f556d0dbee8142e1d22d4aefef7be3aae
Nidal Mohammed <nidalmo23@gmail.com>
2023-05-02 22:23:07 +0530

    small bugs fixed

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -12,18 +12,19 @@ import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { setLogin } from "../../state";
+
 import Dropzone from "react-dropzone";
 import FlexBetween from "../../components/FlexBetween";
+import { setLogin } from "../../state";
 
 const registerSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  password: yup.string().required("required"),
-  location: yup.string().required("required"),
-  occupation: yup.string().required("required"),
-  picture: yup.string().required("required"),
+  firstName: yup.string().required("required") || "",
+  lastName: yup.string().required("required") || "",
+  email: yup.string().email("invalid email").required("required") || "",
+  password: yup.string().required("required") || "",
+  location: yup.string().required("required") || "",
+  occupation: yup.string().required("required") || "",
+  picture: yup.string().required("required") || "",
 });
 
 const loginSchema = yup.object().shape({
